0e4fe28c2b9ce74194e3d7d8a6bf3ee4e401d6f5
ystskm <brilliantpenguin@gmail.com>
2014-11-10 09:46:27 +0900

    jsdom test

--- test/_runner.js
+++ test/_runner.js
@@ -1,3 +1,3 @@
 /***/
-//require('nodeunit-ci').run(['basic', 'jsdom'], __dirname, 5000);
-require('nodeunit-ci').run(['basic'], __dirname, 5000);
+require('nodeunit-ci').run(['basic', 'jsdom'], __dirname, 5000);
+//require('nodeunit-ci').run(['basic'], __dirname, 5000);
